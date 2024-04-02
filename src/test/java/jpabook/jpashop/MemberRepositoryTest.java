cce71924bb94b2a0cfc195228b45b71e8f74abf7
hoho <ghdnjs18@naver.com>
2024-04-02 16:46:49 +0900

    회원 도메인 테스트

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.Member;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,24 +13,25 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest
 class MemberRepositoryTest {
 
-    @Autowired
+//    @Autowired
 //    MemberRepository memberRepository;
 
-    @Test
-    @Transactional
+//    @Test
+//    @Transactional
+//    @Disabled
 //    @Rollback(value = false)
-    public void testMember() throws Exception {
-//        // given
+//    public void testMember() throws Exception {
+        // given
 //        Member member = new Member();
 //        member.setUsername("memberA");
 //
-//        // when
+        // when
 //        Long savedId = memberRepository.save(member);
 //        Member findMember = memberRepository.find(savedId);
 //
-//        // then
+        // then
 //        assertEquals(findMember.getId(), member.getId());
 //        assertEquals(findMember.getUsername(), member.getUsername());
 //        assertEquals(findMember, member);
-    }
+//    }
 }
