3274a5720984bdaf029f120a7ae0dcca9942f999
hoho <ghdnjs18@naver.com>
2024-04-02 21:15:34 +0900

    상품 등록 컨트롤러 및 화면 구현

--- src/main/java/jpabook/jpashop/domain/item/Book.java
+++ src/main/java/jpabook/jpashop/domain/item/Book.java
@@ -3,9 +3,11 @@ package jpabook.jpashop.domain.item;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Getter
+@Setter
 @DiscriminatorValue("B")
 public class Book extends Item {
 
