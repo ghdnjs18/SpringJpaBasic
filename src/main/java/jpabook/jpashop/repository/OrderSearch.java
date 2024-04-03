f1e003ee2aabace859e1a3978c277837271b840e
hoho <ghdnjs18@naver.com>
2024-04-03 14:42:55 +0900

    주문 목록 검색, 취소 컨트롤러 및 화면 구현

--- src/main/java/jpabook/jpashop/repository/OrderSearch.java
+++ src/main/java/jpabook/jpashop/repository/OrderSearch.java
@@ -2,8 +2,10 @@ package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.OrderStatus;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class OrderSearch {
 
     private String memberName;
